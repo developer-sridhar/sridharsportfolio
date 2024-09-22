d92fac42071a75d268a8182129f4ae0d7f779f8d
developer-sridhar <140785207+developer-sridhar@users.noreply.github.com>
2024-09-22 18:44:55 +0530

    Update Intro.js

--- frontend/frontend/src/pages/Home/Intro.js
+++ frontend/frontend/src/pages/Home/Intro.js
@@ -1,27 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-const Intro = () => {
+const Intro = ({ scrollToAbout }) => {
   const { portfolioData } = useSelector((state) => state.root);
-  const { intro } = portfolioData || {}; // Provide a default empty object
+  const { intro } = portfolioData || {}; 
 
-  const { firstName, lastName, welcomeText, description, caption } = intro || {}; // Provide a default empty object
+  const { firstName, lastName, welcomeText, description, caption } = intro || {}; 
 
   return (
-    <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-6 p-6 sm:p-4 md:p-8">
-      <h1 className="text-white text-3xl md:text-2xl">{welcomeText || ''}</h1>
-      <h2 className="text-4xl md:text-3xl sm:text-2xl text-secondary font-bold">
-        {firstName || ''} {lastName || ''}
-      </h2>
-      <h3 className="text-3xl md:text-2xl sm:text-xl text-white font-semibold">
+    <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8">
+      <h1 className="text-white">{welcomeText || ''}</h1>
+      <h2 className="text-7xl sm:text-3xl text-secondary">{firstName || ''} {lastName || ''}</h2>
+      <h3 className="text-6xl sm:text-3xl text-white font-semibold">
         {caption || ''}
       </h3>
-      <p className="text-white text-lg md:text-base sm:text-sm w-full ">
+      <p className="text-white w-2/3">
         {description || ''}
       </p>
-      <button className="border-2 border-tertiary text-tertiary hover:bg-tertiary hover:text-primary px-6 py-2 rounded-md text-base md:text-sm">
-        Get Started
-      </button>
+      <button onClick={scrollToAbout} className="border-2 border-tertiary text-tertiary px-10 py-3 rounded">Get Started</button>
     </div>
   );
 };
